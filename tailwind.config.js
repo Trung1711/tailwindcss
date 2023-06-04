1ec94faf288a9f2056cb6f908e76658d528fe87d
Trung1711 <trannamtrung171199@gmail.com>
2023-06-04 21:56:13 +0700

    first project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        huge: "4rem",
+      },
+      fontFamily: {
+        cherry: ["Cherry Bomb One"],
+      },
+    },
+  },
+  plugins: [],
+};
